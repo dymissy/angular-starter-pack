dddebc88dda6785699042d23a07e8ec6b2c66a70
Simone D'Amico <info@simonedamico.com>
2017-04-08 19:53:52 +0200

    adds prod configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,42 +1,61 @@
 'use strict';
 
 // Modules
-var webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const glob = require('glob');
+const autoprefixer = require('autoprefixer');
 
-var __dist = __dirname + '/dist';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const __dist = __dirname + '/dist';
 
 module.exports = function (env) {
   return {
     entry: {
       'app' : __dirname + '/src/app/root.module.js',
       'vendor' : ['angular'],
+      'style': __dirname + '/src/sass/main.scss',
+      'vendor-style': [
+        __dirname + '/node_modules/angular/angular-csp.css',
+      ],
     },
     output: {
       path: __dist,
-      filename: '[name].[hash].js',
-      chunkFilename: '[name].[hash].js',
+      filename: 'js/[name].[chunkhash].js',
+      chunkFilename: 'js/[name].[chunkhash].js',
+      sourceMapFilename: 'js/[name].[chunkhash].map'
     },
     devtool: 'source-map',
     watch: false,
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loaders: ["babel-loader"],
-          exclude: /node_modules/
-        },
-        {
-          test: /\.scss$/,
-          loaders: ["style-loader", "css-loader", "sass-loader"]
-        }
-      ]
+      rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        }),
+        exclude: __dirname + '/node_modules'
+      }, {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        }),
+        exclude: __dirname + '/src'
+      }]
     },
     plugins: [
       new CopyWebpackPlugin([{
         from: __dirname + '/src/img',
         to: __dist + '/img'
+      }, {
+        from: __dirname + '/src/app/**/*.html',
+        to: __dist,
+        transformPath: function(targetPath, sourcePath) {
+          return targetPath.replace('src/app/', '');
+        }
       }]),
       new HtmlWebpackPlugin({
           template: __dirname + '/src/index.html',
@@ -44,12 +63,18 @@ module.exports = function (env) {
       }),
       new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor',
-        filename: '[name].[hash].js'
+        filename: 'js/[name].[chunkhash].js'
       }),
       new webpack.LoaderOptionsPlugin({
-          minimize: true,
-          debug: false
+        minimize: true,
+        debug: false,
+        options: {
+          postcss: [
+            autoprefixer(),
+          ]
+         }
       }),
+      new ExtractTextPlugin('css/[name].[chunkhash].css'),
       new webpack.optimize.UglifyJsPlugin({
           beautify: false,
           mangle: {
