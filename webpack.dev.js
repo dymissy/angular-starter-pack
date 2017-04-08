767c133c2c1768627bfcb953f0f0028cef8a4661
Simone D'Amico <info@simonedamico.com>
2017-04-08 18:23:27 +0200

    compile sass files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,38 @@
 'use strict';
 
 // Modules
-var webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const glob = require('glob');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var __dist = __dirname + '/dist';
+const __dist = __dirname + '/dist';
 
 module.exports = function (env) {
   return {
     entry: {
       'app' : __dirname + '/src/app/root.module.js',
       'vendor' : ['angular'],
+      'style': glob.sync(__dirname + '/src/sass/**/*.scss'),
     },
     output: {
       path: __dist,
-      filename: '[name].bundle.js',
-      chunkFilename: '[name].bundle.js',
-      sourceMapFilename: '[name].map'
+      filename: 'js/[name].bundle.js',
+      chunkFilename: 'js/[name].bundle.js',
+      sourceMapFilename: 'js/[name].map'
     },
     devtool: 'cheap-module-source-map',
     watch: true,
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }]
+    },
     plugins: [
       new CopyWebpackPlugin([{
         from: __dirname + '/src/img',
@@ -38,8 +50,9 @@ module.exports = function (env) {
       }),
       new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor',
-        filename: '[name].bundle.js'
+        filename: 'js/[name].bundle.js'
       }),
+      new ExtractTextPlugin('css/[name].bundle.css')
     ],
     devServer: {
       contentBase: __dist,
